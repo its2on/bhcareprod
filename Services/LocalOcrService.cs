74f4379117bf8f24c57f89d1e9604115147a56cf
its2on <lopez.286922@novaliches.sti.edu.ph>
2025-11-22 10:07:47 +0800

    New thingy

--- Services/LocalOcrService.cs
+++ Services/LocalOcrService.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
